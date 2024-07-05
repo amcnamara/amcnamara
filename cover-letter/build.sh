279a0e3a652ccf8f12e3c05d462a92104e345bc9
Alex McNamara <alex.mcnamara@gmail.com>
2024-07-04 22:29:00 -0700

    Silence terminal noise unless a build fails.

--- cover-letter/build.sh
+++ cover-letter/build.sh
@@ -5,11 +5,16 @@ function run() {
         brew install inkscape
     fi
 
+    if ! type "chronic" > /dev/null; then
+        brew install moreutils
+    fi
+
     local files=$(find ./*.tex)
 
     echo $files | while read -r file _; do
         echo "building: $fg[yellow]$file$reset_color"
-        pdflatex --shell-escape -output-directory ./artifacts $file
+        # chronic will suppress terminal noise unless the build fails
+        chronic pdflatex --shell-escape -output-directory ./artifacts $file
     done
 
     echo -n "cleaning up sub-artifacts... "
