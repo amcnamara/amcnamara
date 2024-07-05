71a57c8bce953c2edca31d5b33c34fbbd5fb9921
Alex McNamara <alex.mcnamara@gmail.com>
2024-07-04 22:36:11 -0700

    Fix colors on echo.

--- cover-letter/build.sh
+++ cover-letter/build.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
 function run() {
+    autoload colors; colors
+
     if ! type "inkscape" > /dev/null; then
         brew install inkscape
     fi
@@ -12,7 +14,7 @@ function run() {
     local files=$(find ./*.tex)
 
     echo $files | while read -r file _; do
-        echo "building: $fg[yellow]$file$reset_color"
+        echo "building: $file"
         # chronic will suppress terminal noise unless the build fails
         chronic pdflatex --shell-escape -output-directory ./artifacts $file
     done
